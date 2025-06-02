c3f9be46325e81578530adfdb92a51e41f16041c
Justin Lerner <justin@jlern.com>
2025-06-01 18:43:55 -0700

    before graphql adjustments

--- components/UI/ArrowDown.vue
+++ components/UI/ArrowDown.vue
@@ -1,11 +1,24 @@
 <script setup>
 const props = defineProps(["theme"]);
+const arrow = ref(null);
+
+onMounted(() => {
+  // sticky nav
+  useEventListener(window, "scroll", () => {
+    if (window.scrollY > 0) {
+      arrow.value.classList.add("off");
+    } else {
+      arrow.value.classList.remove("off");
+    }
+  });
+});
 </script>
 
 <template>
   <!-- arrow down -->
   <div
-    class="arrow-down absolute left-0 top-[100vh] z-5 -mt-[8.2rem] w-[1.5rem] pl-side"
+    class="arrow-down fixed left-0 top-[100vh] z-5 -mt-[8.2rem] w-[1.5rem] pl-side"
+    ref="arrow"
   >
     <IconArrow
       :color="theme == 'dark' ? 'stroke-white' : 'stroke-black'"
@@ -14,4 +27,12 @@ const props = defineProps(["theme"]);
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.arrow-down {
+  transition: var(--ease-out);
+  &.off {
+    transition-timing-function: cubic-bezier(1, 0, 0.5, 1);
+    transform: translateX(-8rem);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <script setup>
-import HomeQuery from "~/assets/graphql/home.graphql";
+//import HomeQuery from "~/assets/graphql/home.graphql";
 const theme = useState("theme");
 theme.value = "light";
 
-const QUERY = HomeQuery.loc.source.body;
+import * as file from "~/assets/graphql/home.graphql";
+console.log(file);
+//console.log(HomeQuery.loc.source.body);
+
+/* const QUERY = HomeQuery.loc.source.body;
 const { data } = await useGraphqlQuery({
   query: QUERY,
 });
-const page = data.value.home;
+const page = data.value.home; */
 </script>
 
 <template>
   <div class="bg-skyblue">
-    <HomeHero :data="page.hero" />
+    <!-- <HomeHero :data="page.hero" /> -->
     <HomeIntro />
     <OverviewCards theme="dark" :nav="true" />
     <SliderAscending />
