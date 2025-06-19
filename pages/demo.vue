c89ccdd47aef9ba53e52cc3abe0944417c073baa
Justin Lerner <justin@jlern.com>
2025-06-19 13:27:48 -0700

    added hubspot form code to demo page

--- pages/demo.vue
+++ pages/demo.vue
@@ -6,8 +6,36 @@ const { data } = await useGraphqlQuery({
   query: demoQuery.loc.source.body,
 });
 const page = data.value.bookDemo;
+const form_id = "5350cf07-c779-4320-9bd6-a3ecc0bef59f";
 
 onMounted(() => {
+  window.addEventListener("message", (event) => {
+    if (
+      event.data.type === "hsFormCallback" &&
+      event.data.eventName === "onFormReady"
+    ) {
+      nextTick(() => {
+        loaded.value = true;
+      });
+    }
+  });
+
+  if (form_id) {
+    const script = document.createElement("script");
+    script.src = "https://js.hsforms.net/forms/v2.js";
+    document.body.appendChild(script);
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "48112164",
+          formId: form_id,
+          target: "#hubspotForm",
+          css: "",
+        });
+      }
+    });
+  }
+
   const theme = useState("theme");
   theme.value = "light";
 });
@@ -90,55 +118,7 @@ useHead(() => {
               id="hubspotForm"
               v-once
               class="[&_.hs-input]:leading-normal [&_input[type='submit']:hover]:text-lime [&_input[type='submit']]:leading-normal relative [&_.hs-form-field]:flex-1 [&_.hs-form-field_label]:text-body-sm [&_.hs-form-field_label]:text-white [&_.hs-form-field_label]:opacity-50 [&_.hs-input]:mt-3 [&_.hs-input]:h-[5rem] [&_.hs-input]:w-full [&_.hs-input]:rounded-sm [&_.hs-input]:border-1 [&_.hs-input]:border-whiteline [&_.hs-input]:bg-shadowblue [&_.hs-input]:!px-6 [&_.hs-input]:!pb-[1.125rem] [&_.hs-input]:!pt-[1.3125rem] [&_.hs-input]:text-body-sm [&_.hs-input]:text-white [&_.hs-input]:outline-none [&_.hs-input]:s:mt-3 s:[&_.hs-input]:!px-[1.875rem] [&_fieldset]:w-full [&_fieldset]:gap-[3.5rem] [&_fieldset]:s:flex [&_form]:space-y-[3rem] [&_input[type='checkbox']]:mt-0 [&_input[type='checkbox']]:h-auto [&_input[type='submit']]:mt-[1rem] [&_input[type='submit']]:w-auto [&_input[type='submit']]:min-w-[13.5rem] [&_input[type='submit']]:rounded-btn [&_input[type='submit']]:bg-red [&_input[type='submit']]:!px-[1.4rem] [&_input[type='submit']]:!py-[1.4rem] [&_input[type='submit']]:font-helvh [&_input[type='submit']]:text-body-sm [&_input[type='submit']]:text-skyblue [&_select]:appearance-none"
-            >
-              <form>
-                <fieldset class="form-columns-2">
-                  <div
-                    class="hs_firstname hs-firstname hs-fieldtype-text field hs-form-field"
-                  >
-                    <label><span>First Name*</span></label>
-                    <div class="input">
-                      <input name="firstname" type="text" class="hs-input" />
-                    </div>
-                  </div>
-                  <div
-                    class="hs_lastname hs-lastname hs-fieldtype-text field hs-form-field"
-                  >
-                    <label><span>Last Name*</span></label>
-                    <div class="input">
-                      <input name="lastname" type="text" class="hs-input" />
-                    </div>
-                  </div>
-                </fieldset>
-                <fieldset>
-                  <div
-                    class="hs_firstname hs-firstname hs-fieldtype-text field hs-form-field"
-                  >
-                    <label><span>Email*</span></label>
-                    <div class="input">
-                      <input name="firstname" type="email" class="hs-input" />
-                    </div>
-                  </div>
-                </fieldset>
-                <fieldset>
-                  <div
-                    class="hs_firstname hs-firstname hs-fieldtype-text field hs-form-field"
-                  >
-                    <label><span>Company Name*</span></label>
-                    <div class="input">
-                      <input name="firstname" type="email" class="hs-input" />
-                    </div>
-                  </div>
-                </fieldset>
-                <div class="hs_submit hs-submit">
-                  <input
-                    type="submit"
-                    class="hs-button primary large"
-                    value="Submit"
-                  />
-                </div>
-              </form>
-            </div>
+            ></div>
             <!-- end hubspot form -->
           </div>
         </div>
