0439d759ac79580dbbf262edabfdacdfbf34feeb
Justin Lerner <justin@jlern.com>
2025-06-20 10:14:36 -0700

    fixed overview cards intro

--- components/OverviewCards.vue
+++ components/OverviewCards.vue
@@ -3,13 +3,11 @@ const props = defineProps(["theme", "header", "nav", "data"]);
 </script>
 
 <template>
-  <Section :theme="theme" side="none" :class="!header && '!pt-0'">
+  <Section :theme="theme" side="none">
     <SectionHeader
-      v-if="header"
-      class="border-b px-side-mob pb-[11.5rem] s:px-side"
-      :class="theme == 'dark' ? 'border-[#3D4856] bg-shadowblue' : null"
-      :theme="theme"
+      class="border-b border-[#3D4856] bg-shadowblue px-side-mob pb-[11.5rem] s:px-side"
       :data="data.header"
+      theme="dark"
       ><UIGradientBot :theme="theme"
     /></SectionHeader>
 
