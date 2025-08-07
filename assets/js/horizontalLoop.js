fc824011457461dc9eaf5313498eb772b8b9464f
Justin Lerner <justin@jlern.com>
2025-08-06 22:25:38 -0700

    testing carousel debugging

--- assets/js/horizontalLoop.js
+++ assets/js/horizontalLoop.js
@@ -249,7 +249,6 @@ export default function horizontalLoop(items, config) {
         onThrowUpdate: align,
         overshootTolerance: 0,
         inertia: true,
-        dragResistance: 0.6,
         snap(value) {
           //note: if the user presses and releases in the middle of a throw, due to the sudden correction of proxy.x in the onPressInit(), the velocity could be very large, throwing off the snap. So sense that condition and adjust for it. We also need to set overshootTolerance to 0 to prevent the inertia from causing it to shoot past and come back
           if (Math.abs(startProgress / -ratio - this.x) < 10) {
