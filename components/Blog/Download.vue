ce75feb9b4dc562c4c9ec1e060fca3e3ffe13cf4
Justin Lerner <justin@jlern.com>
2025-09-23 14:19:19 -0700

    added download block to blog

---
+++ components/Blog/Download.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps(["data"]);
+
+const formattedUrl = computed(() => {
+  return props.data.file.url + "?dl=" + props.data.file.filename;
+});
+</script>
+
+<template>
+  <div class="content-block blog-download w-full [&_*+*]:!mt-0">
+    <CtaBtn :to="formattedUrl" download>
+      {{ data.text }}
+    </CtaBtn>
+  </div>
+</template>
+
+<style scoped></style>
