bdb099c0282ffad62c0bc991d0466aa0e2829c55
Justin Lerner <justin@jlern.com>
2026-02-09 14:27:20 -0800

    added vimeo capability for blog video embed

--- components/Blog/ThumbImage.vue
+++ components/Blog/ThumbImage.vue
@@ -26,31 +26,57 @@ defineExpose({
 <template>
   <div
     class="thumb-img dato-image relative z-1 overflow-hidden rounded-base-mob s:rounded-base [&_img]:h-full [&_img]:w-full [&_img]:object-cover"
-    :class="loc == 'live-demo' ? '!aspect-[1.77]' : '!aspect-[1.3]'">
+    :class="loc == 'live-demo' ? '!aspect-[1.77]' : '!aspect-[1.3]'"
+  >
     <!-- blog thumbnail -->
-    <DatocmsImage v-if="data.image" :data="data.image.responsiveImage" class="h-full w-full" />
+    <DatocmsImage
+      v-if="data.image"
+      :data="data.image.responsiveImage"
+      class="h-full w-full"
+    />
 
     <!-- media thumbnail -->
-    <DatocmsImage v-else-if="data.media?.image" :data="data.media.image.responsiveImage" class="h-full w-full" />
+    <DatocmsImage
+      v-else-if="data.media?.image"
+      :data="data.media.image.responsiveImage"
+      class="h-full w-full"
+    />
 
     <!-- live demo thumbnail -->
-    <DatocmsImage v-else-if="data.media?.screen && loc == 'live-demo'" :data="data.media.screen.responsiveImage"
-      class="h-full w-full" />
+    <DatocmsImage
+      v-else-if="data.media?.screen && loc == 'live-demo'"
+      :data="data.media.screen.responsiveImage"
+      class="h-full w-full"
+    />
 
     <!-- texture background fallback -->
-    <img v-else src="/video/media-bg.jpg" class="absolute left-0 top-0 size-full" />
+    <img
+      v-else
+      src="/video/media-bg.jpg"
+      class="absolute left-0 top-0 size-full"
+    />
 
     <!-- product demo -->
-    <div v-if="data.media?.__typename == 'ProductDemoRecord' && loc != 'live-demo'"
-      class="absolute left-0 top-0 flex size-full items-center p-[6rem]">
+    <div
+      v-if="data.media?.__typename == 'ProductDemoRecord' && loc != 'live-demo'"
+      class="absolute left-0 top-0 flex size-full items-center p-[6rem]"
+    >
       <div class="relative overflow-hidden rounded-base shadow-media">
-        <DatocmsImage v-if="data.media.screen" :data="data.media.screen.responsiveImage" />
-        <div class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"></div>
+        <DatocmsImage
+          v-if="data.media.screen"
+          :data="data.media.screen.responsiveImage"
+        />
+        <!-- <div
+          class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"
+        ></div> -->
       </div>
     </div>
 
     <!-- has video loop -->
-    <div v-if="data.media?.video?.loopClip" class="absolute left-0 top-0 size-full">
+    <div
+      v-if="data.media?.video?.loopClip"
+      class="absolute left-0 top-0 size-full"
+    >
       <video loop muted autoplay playsinline class="size-full object-contain">
         <source :src="data.media.video.loopClip.url" type="video/mp4" />
       </video>
@@ -58,11 +84,11 @@ defineExpose({
 
     <!-- video -->
     <template v-if="isVideo">
-      <div v-if="
+      <!-- <div v-if="
         data.media?.__typename == 'MediaVideoRecord' ||
         data.media?.video?.loopClip
         || loc == 'live-demo'
-      " class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"></div>
+      " class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"></div> -->
 
       <!-- button -->
       <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -71,23 +97,32 @@ defineExpose({
     </template>
 
     <!-- podcast -->
-    <div v-if="data.media?.__typename == 'PodcastRecord'"
-      class="absolute left-0 top-0 flex size-full items-center justify-center">
+    <div
+      v-if="data.media?.__typename == 'PodcastRecord'"
+      class="absolute left-0 top-0 flex size-full items-center justify-center"
+    >
       <MediaPodcast :data="data" />
     </div>
 
     <!-- ebook -->
-    <div v-if="data.media?.__typename == 'EbookRecord'"
-      class="absolute left-0 top-0 flex size-full items-center justify-center">
+    <div
+      v-if="data.media?.__typename == 'EbookRecord'"
+      class="absolute left-0 top-0 flex size-full items-center justify-center"
+    >
       <MediaEbook :data="data" />
     </div>
 
     <!-- press -->
     <template v-if="data.tag?.slug == 'press' && !isVideo">
-      <div class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"></div>
+      <div
+        class="absolute left-0 top-0 size-full bg-[#2A3440] opacity-80"
+      ></div>
 
       <!-- logo -->
-      <div v-if="data.logo" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        v-if="data.logo"
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
         <div class="h-[10rem] w-[20rem]">
           <img :src="data.logo.url" class="!h-full !w-full !object-contain" />
         </div>
@@ -95,9 +130,14 @@ defineExpose({
     </template>
 
     <!-- collaborations -->
-    <div v-if="data.tag?.slug == 'collaborations'"
-      class="absolute left-1/2 top-1/2 h-[14.4rem] w-[29rem] -translate-x-1/2 -translate-y-1/2 rounded-base bg-jaffa">
-      <div v-if="data.logo" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div
+      v-if="data.tag?.slug == 'collaborations'"
+      class="absolute left-1/2 top-1/2 h-[14.4rem] w-[29rem] -translate-x-1/2 -translate-y-1/2 rounded-base bg-jaffa"
+    >
+      <div
+        v-if="data.logo"
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
         <div class="h-[8rem] w-[16rem]">
           <img :src="data.logo.url" class="!h-full !w-full !object-contain" />
         </div>
